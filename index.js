05a87e6f7fb60591058f450f99bd9faf517a3cd0
Rahul <chandrahul442@gmail.com>
2022-02-02 12:31:19 +0545

    register

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const hbs = require('hbs')
 
 const app = express();
 require('./db/conn');
+const Register = require('./models/register')
 
 const static_path = path.join(__dirname, '../public')
 const template = path.join(__dirname, './templates/views')
@@ -13,8 +14,52 @@ app.set('view engine','hbs');
 app.set('views',template);
 hbs.registerPartials(partials_path)
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
 app.get('/', (req,res)=>{
     res.render('login')
 });
+app.get('/register', (req,res)=>{
+    res.render('register')
+});
+
+
+app.get('/login', (req,res)=>{
+    res.render('login')
+});
+
+app.post('/register', async(req,res)=>{
+    try{
+        // console.log(req.body.fullname)
+        const password= req.body.password;
+        const cpassword= req.body.conpassword;
+        if(password===cpassword){
+            const registerUser = new Register({
+                fullname: req.body.fullname,
+                email: req.body.email,
+                phone: req.body.phone,
+                password: req.body.password,
+                confirmpassword: req.body.conpassword,
+            })
+            const registered = await registerUser.save();
+            res.status(201).render('index')
+        }else{
+            res.send('Password not matched')
+        }
+    }catch(error){
+        res.status(400).send(error)
+    }
+});
+
+app.post('/login',async(req,res)=>{
+    try{
+        
+        const password = req.body.password;
+    } catch(error){
+        res.status(400).send("Invalid Credentials")
+    }
+
+})
 
 app.listen(3000)
---
+++ models/register.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const regSchema = new mongoose.Schema({
+    fullname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    confirmpassword:{
+        type:String,
+        required:true
+    }
+})
+
+const  Register = new mongoose.model('Register',regSchema)
+
+module.exports = Register;
